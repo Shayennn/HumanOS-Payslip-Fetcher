466a9a4ce7d8a2473bcd65fa4bb38a46749ebe37
Phitchawat Lukkanathiti <725-phitchawat.l@users.noreply.git.wndv.co>
2024-04-30 19:51:57 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,113 @@
+from datetime import datetime
+from pprint import pprint
+import requests
+
+APP_VERSION = "8.48"
+
+session = requests.Session()
+session.headers.update(
+    {
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+        "Accept": "*/*",
+        "LanguageID": "2",
+        "Accept-Language": "en;q=1.0",
+        "User-Agent": f"HumanOS/${APP_VERSION} (com.itcat.HumanOS; build:1; iOS 17.4.1) Alamofire/${APP_VERSION}",
+        "DeviceOSType": "2",
+        "AppVersion": APP_VERSION,
+    }
+)
+
+def login(user, password, firebase_token=None):
+    if firebase_token is None:
+        firebase_token = ""
+    data = {
+        "AppVersion": APP_VERSION,
+        "DeviceName": "iPhone12,5",
+        "FirebaseToken": firebase_token,
+        "Password": password,
+        "TimeZone": "420",
+        "UserName": user,
+    }
+    url = "https://api.humanos.biz/api/v35.0/Main/PreLogin"
+    response = session.post(url, data=data)
+    res_data = response.json()
+    if not res_data["Success"]:
+        raise Exception(res_data["Result"])
+    session.headers.update(
+        {
+            "Accept": "*/*",
+            "UserID": f"{res_data["Data"]["UserID"]}",
+            "AppVersion": APP_VERSION,
+            "ClientID": f"{res_data["Data"]["ClientID"]}",
+            "Accept-Language": "en;q=1.0",
+            "Token": f"{res_data["Data"]["Token"]}",
+            "DeviceOSType": "2",
+            "User-Agent": f"HumanOS/${APP_VERSION} (com.itcat.HumanOS; build:1; iOS 17.4.1) Alamofire/${APP_VERSION}",
+            "PlatformType": "2",
+            "TimeZone": "420",
+            "LanguageID": "2",
+        }
+    )
+    return res_data["Data"]
+
+def get_payslips(year=None):
+    if year is None:
+        year = datetime.now().year
+    url = f"https://api.humanos.biz/api/v35.0/PaySlip/GetPaySlipList"
+    response = session.get(url, params={
+        "onYear": year
+    })
+    res_data = response.json()
+    if not res_data["Success"]:
+        raise Exception(res_data["Result"])
+    return res_data["Data"]
+
+def get_payslip(installment_id):
+    url = f"https://api.humanos.biz/api/v35.0/PaySlip/GetPaySlipDetail"
+    response = session.get(url, params={
+        "installmentID": installment_id
+    })
+    res_data = response.json()
+    if not res_data["Success"]:
+        raise Exception(res_data["Result"])
+    return res_data["Data"]
+
+def get_payslip_pdf(installment_id):
+    url = f"https://api.humanos.biz/api/v35.0/PaySlip/DownloadPaySlip"
+    response = session.get(url, params={
+        "installmentID": installment_id
+    })
+    pdf = response.content
+    pdf_name = response.headers["Content-Disposition"].split("filename=")[1].replace('/', "_").replace('\\', "_")
+    return (pdf_name, pdf)
+
+def logout():
+    url = "https://api.humanos.biz/api/v35.0/Main/Logout"
+    response = session.post(url)
+    res_data = response.json()
+    if not res_data["Success"]:
+        raise Exception(res_data["Result"])
+    return res_data["Result"]
+
+
+if __name__ == "__main__":
+    # load user password token from environment variable
+    dotenv.load_dotenv()
+    user = os.getenv("USER")
+    password = os.getenv("PASSWORD")
+    firebase_token = os.getenv("FIREBASE_TOKEN")
+
+    data = login(user, password, firebase_token)
+    print(f"Logged in as {data["MyContact"][0]["FName"]} {data["MyContact"][0]["LName"]} success")
+    try:
+        payslips = get_payslips()
+        pprint(payslips)
+        payslip = get_payslip(payslips['Data'][-1]["InstallmentID"])
+        pprint(payslip)
+        pdf_name, pdf = get_payslip_pdf(payslips['Data'][-1]["InstallmentID"])
+        with open(pdf_name, "wb") as f:
+            f.write(pdf)
+    except Exception as e:
+        pprint(e)
+    finally:
+        pprint(logout())
